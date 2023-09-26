05838c7d1fdab1ff3fa6a267a921ef99c2175668
juansedev2 <jsab62tol@gmail.com>
2023-09-26 17:24:42 -0500

    Add the list of the order of the results and better styles

--- public/views/Index.view.php
+++ public/views/Index.view.php
@@ -1,6 +1,6 @@
 <?php include_once "./public/views/partials/Header.partial.php"?>
 <div class="container">
-    <h1>Web Scraper</h1>
+    <h1>Web Scraper para comparación de precios</h1>
     <?php include_once "./public/views/partials/FormProduct.partial.php"?>
 </div>
 <?php include_once "./public/views/partials/Footer.partial.php"?>
--- public/views/partials/FormProduct.partial.php
+++ public/views/partials/FormProduct.partial.php
@@ -1,6 +1,15 @@
 <form action="search" method="GET">
     <div class="input-group input-group-lg">
-        <input type="text" name="keywords" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Escribe lo que deseas buscar...">
+        <input type="text" name="keywords" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Escribe lo que deseas buscar..." value="<?= $_GET["keywords"] ?? "" ?>">
         <button type="submit" class="btn btn-light">Buscar</button>
     </div>
+    <?php if(isset($_GET["keywords"])):?>
+        <div class="m-3 w-25">
+            <select class="form-select" aria-label="Default select example">
+            <option value="normal">Normal</option>
+            <option value="asc">De menor a mayor precio</option>
+            <option value="dsc">De mayor a menor precio</option>
+            </select>
+        </div>    
+    <?php endif;?>
 </form>
