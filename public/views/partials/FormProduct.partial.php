97ca88fd67010b0fbb158cae7585a99bcf5558b2
juansedev2 <jsab62tol@gmail.com>
2023-09-26 17:42:43 -0500

    Improved the dinamic list of the options in the form of the order of scraping

--- public/views/partials/FormProduct.partial.php
+++ public/views/partials/FormProduct.partial.php
@@ -3,13 +3,17 @@
         <input type="text" name="keywords" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Escribe lo que deseas buscar..." value="<?= $_GET["keywords"] ?? "" ?>">
         <button type="submit" class="btn btn-light">Buscar</button>
     </div>
-    <?php if(isset($_GET["keywords"])):?>
-        <div class="m-3 w-25">
-            <select class="form-select" aria-label="Default select example">
-            <option value="normal">Normal</option>
-            <option value="asc">De menor a mayor precio</option>
-            <option value="dsc">De mayor a menor precio</option>
-            </select>
-        </div>    
-    <?php endif;?>
+    <div class="m-3 w-25">
+        <select class="form-select" aria-label="Default select example" name="order">
+            <?php if(isset($_GET["order"])) :?>
+                <option <?= $_GET["order"] == "normal" ? "selected" : "" ?> value="normal">Normal</option>
+                <option <?= $_GET["order"] == "asc" ? "selected" : " " ?> value="asc">De menor a mayor precio</option>
+                <option <?= $_GET["order"] == "dsc" ? "selected" : " " ?> value="dsc">De mayor a menor precio</option>
+            <?php else:?>
+                <option selected value="normal">Normal</option>
+                <option value="asc">De menor a mayor precio</option>
+                <option value="dsc">De mayor a menor precio</option>
+            <?php endif;?>
+        </select>
+    </div>    
 </form>
